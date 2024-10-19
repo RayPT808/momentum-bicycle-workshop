abb52f77620d37041bfe427d50bd9cd14ea0b7c1
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2024-10-19 12:02:14 +0000

    Changed registration form rendering.

--- momentum_bicycle_workshop/urls.py
+++ momentum_bicycle_workshop/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include  # Ensure 'include' is imported
+from . import views
 
 urlpatterns = [
     path('', include('myapp.urls')), 
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # Include authentication URLs
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),    # Include authentication URLs
 ]
--- myapp/forms.py
+++ myapp/forms.py
@@ -5,7 +5,7 @@ from .models import Appointment
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-    password_confirm = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
 
     class Meta:
         model = User
@@ -16,15 +16,9 @@ class UserRegistrationForm(forms.ModelForm):
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
 
-        if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError("Passwords do not match.")
+        if password != password_confirm:
+            raise forms.ValidationError("Passwords do not match")
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])  # This hashes the password
-        if commit:
-            user.save()
-        return user
 
 
 class AppointmentForm(forms.ModelForm):
