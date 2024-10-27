63adea49c35dafc12eadbf3dd2ac947b79978fb3
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2024-10-27 13:22:12 +0000

    Favicon added to all pages.

--- momentum_bicycle_workshop/urls.py
+++ momentum_bicycle_workshop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin 
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +12,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('', include('myapp.urls')),
+
     # Add other paths as needed
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
