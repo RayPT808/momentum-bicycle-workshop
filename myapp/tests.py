86946f4a0dd48665d8f70499494fd19427b4a867
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-02-25 15:01:25 +0000

    Added test for home page login function.

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_status_code(self):
+        """Test if home page loads successfully"""
+        response = self.client.get(reverse('home'))  
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_login_success(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        self.assertEqual(response.status_code, 302)  # Expect redirect on successful login
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
