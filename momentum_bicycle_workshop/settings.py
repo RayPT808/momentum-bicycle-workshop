e6a42c8bf5fe40a3cfde678e0c0815f9f7b4be0e
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-03-02 20:13:28 +0000

    Static dir setting changed in settings.py.

--- momentum_bicycle_workshop/settings.py
+++ momentum_bicycle_workshop/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 # Static files 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
